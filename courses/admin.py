f86dcd3b606a21616fd5e3e60ec76370054a5783
Zeki Burak Özden <zburakozden@gmail.com>
2024-01-23 14:36:46 +0300

    kurs bilgilerimizin admin panel uzerindeki goruntu ayarlarini yapmak icin bazi ayarlar ekledik.

--- courses/admin.py
+++ courses/admin.py
@@ -3,5 +3,44 @@ from .models import Course, Category
 
 # Register your models here.
 
-admin.site.register(Course)
-admin.site.register(Category)
+@admin.register(Course) # bu ifade asagidaki admin.site.register(Course, CourseAdmin) ifadesi tanimlamak anlamina gelir.
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        "title", 
+        "isActive",
+        "slug",
+    )
+
+    list_display_links = (
+        "title",
+    )
+
+    readonly_fields = (
+        "slug",
+    )
+
+    list_filter = (
+        "isActive",
+    )
+
+    list_editable = (
+        "isActive",
+    )
+
+    search_fields = (
+        "title",
+        "description",
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
+
+# admin.site.register(Course)
+# admin.site.register(Category)
+# Bu ifadeler yerine yukarida decorator kullandik.
