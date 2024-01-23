915d405f4cc7453730219ff97e041427a685764b
Zeki Burak Özden <zburakozden@gmail.com>
2024-01-23 13:35:50 +0300

    Modellerimizi admin panel uzerinde gorebilmek icin admin.py dosyamiza gerekli kodlari ekledik ve slug ifadesi otomatik olarak olusturulacagi icin admin panel uzerindeki veri girisini zorunlu olmaktan cikartmak icin slug tanimladigimiz satira blank=True ifadesini ekledik, sonra bu alanin admin panel uzerinden duzenlenmesi sacma diyerek editable=False ifadesi ile duzenlenebilirligini kaldirdik.

--- courses/models.py
+++ courses/models.py
@@ -8,7 +8,7 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50, blank=False)
     date = models.DateField()
     isActive = models.BooleanField()
-    slug = models.SlugField(default = "", null = False, unique=True, db_index=True)
+    slug = models.SlugField(default="", blank=True, editable=False, null=False, unique=True, db_index=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
