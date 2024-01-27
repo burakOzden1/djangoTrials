e1ec9b0ef57e23e80ee689e427e1608f5b576b9f
Zeki Burak Özden <zburakozden@gmail.com>
2024-01-27 12:39:34 +0300

    pagination islemleri icin views dosyasi uzerinde bir takim islemler yapildi.

--- courses/views.py
+++ courses/views.py
@@ -4,50 +4,51 @@ from django.urls import reverse
 from datetime import date, datetime
 from .models import Course
 from .models import Category
+from django.core.paginator import Paginator
 
 
-data = {
-    "programlama":"programlama kategorisine ait kurslar",
-    "web-gelistirme":"Web gelistirme kategorisine ait kurslar",
-    "mobil":"Mobil kategorisine ait kurslar",
-}
+# data = {
+#     "programlama":"programlama kategorisine ait kurslar",
+#     "web-gelistirme":"Web gelistirme kategorisine ait kurslar",
+#     "mobil":"Mobil kategorisine ait kurslar",
+# }
 
-db = {
-    "courses": [
-        {
-            "title": "JavaScript Kursu",
-            "description": "JavaScript Kurs açıklaması, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla nihil nam corrupti dolores exercitationem alias omnis recusandae quam id asperiores.",
-            "imageUrl": f"1.jpg",
-            "slug": "javascript-kursu",
-            "date": datetime.now(),
-            "isActive": True,
-            "isUpdated": False,
-        },
-        {
-            "title": "Python Kursu",
-            "description": "Python Kurs açıklaması, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla nihil nam corrupti dolores exercitationem alias omnis recusandae quam id asperiores.",
-            "imageUrl": f"2.jpg",
-            "slug": "python-kursu",
-            "date": date(2022, 9, 10),
-            "isActive": True,
-            "isUpdated": False,
-        },
-        {
-            "title": "Web Geliştirme Kursu",
-            "description": "Web Geliştirme Kurs açıklaması, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla nihil nam corrupti dolores exercitationem alias omnis recusandae quam id asperiores.",
-            "imageUrl": f"3.jpg",
-            "slug": "web-gelistirme-kursu",
-            "date": date(2022, 8, 10),
-            "isActive": True,
-            "isUpdated": True,
-        },
-    ],
-    "categories": [
-        {"id": 1, "name": "Programlama", "slug": "programlama"},
-        {"id": 2, "name": "Web Geliştirme", "slug": "web-gelistirme"},
-        {"id": 3, "name": "Mobil Uygulamalar", "slug": "mobil-uygulamalar"},
-    ]
-}
+# db = {
+#     "courses": [
+#         {
+#             "title": "JavaScript Kursu",
+#             "description": "JavaScript Kurs açıklaması, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla nihil nam corrupti dolores exercitationem alias omnis recusandae quam id asperiores.",
+#             "imageUrl": f"1.jpg",
+#             "slug": "javascript-kursu",
+#             "date": datetime.now(),
+#             "isActive": True,
+#             "isUpdated": False,
+#         },
+#         {
+#             "title": "Python Kursu",
+#             "description": "Python Kurs açıklaması, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla nihil nam corrupti dolores exercitationem alias omnis recusandae quam id asperiores.",
+#             "imageUrl": f"2.jpg",
+#             "slug": "python-kursu",
+#             "date": date(2022, 9, 10),
+#             "isActive": True,
+#             "isUpdated": False,
+#         },
+#         {
+#             "title": "Web Geliştirme Kursu",
+#             "description": "Web Geliştirme Kurs açıklaması, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla nihil nam corrupti dolores exercitationem alias omnis recusandae quam id asperiores.",
+#             "imageUrl": f"3.jpg",
+#             "slug": "web-gelistirme-kursu",
+#             "date": date(2022, 8, 10),
+#             "isActive": True,
+#             "isUpdated": True,
+#         },
+#     ],
+#     "categories": [
+#         {"id": 1, "name": "Programlama", "slug": "programlama"},
+#         {"id": 2, "name": "Web Geliştirme", "slug": "web-gelistirme"},
+#         {"id": 3, "name": "Mobil Uygulamalar", "slug": "mobil-uygulamalar"},
+#     ]
+# }
 
 
 def index(request):
@@ -80,12 +81,19 @@ def details(request, slug):
 #     return HttpResponse(f"{ category } kategorisindeki kurs listesi")
 
 def getCoursesByCategory(request, slug):
-    kurslar = Course.objects.filter(categories__slug=slug, isActive=True)
+    kurslar = Course.objects.filter(categories__slug=slug, isActive=True).order_by("date") # tarihe gore siralamasini soyledik.
     kategoriler = Category.objects.all()
+
+    paginator = Paginator(kurslar, 2) # her sayfada 2 adet kurs olsun
+    page = request.GET.get("page", 1) # varsayilan olarak ilk sayfayi al
+    courses = paginator.get_page(page) # page sayfasindaki urun bilgilerini getir.
+    # print(paginator.count) # kurs sayisini verir.
+    # print(paginator.num_pages) # sayfa sayisini verir.
+
 
     context = dict(
         categories = kategoriler,
-        courses = kurslar,
+        courses = courses,
         seciliKategori = slug,
     )
     return render(request, 'courses/index.html', context)
