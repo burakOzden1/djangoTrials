6581f8bda741f8b15bad499f5a4c98c9206d8890
Zeki Burak Özden <zburakozden@gmail.com>
2024-02-01 22:08:45 +0300

    django form fields dokumanindan bazi ozellikler eklendi.

--- courses/forms.py
+++ courses/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
 class CourseCreateForm(forms.Form):
-    title = forms.CharField()
+    title = forms.CharField(
+        label="Kurs Başlığı",
+        required=True,
+        error_messages={
+            "required": "Kurs başlığı alanı zorunludur.",
+        }
+    )
     description = forms.CharField(widget=forms.Textarea)
     imageUrl = forms.CharField()
     slug = forms.SlugField()
