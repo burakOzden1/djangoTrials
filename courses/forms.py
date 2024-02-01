df83dada74196a281206a6b9189705c142ba8046
Zeki Burak Özden <zburakozden@gmail.com>
2024-02-01 23:20:47 +0300

    form alaninda bazi duzenlemeler. Daha sonra ayrintili sekilde incelenecek.

--- courses/forms.py
+++ courses/forms.py
@@ -5,20 +5,10 @@ class CourseCreateForm(forms.Form):
         label="Kurs Başlığı",
         required=True,
         error_messages={
-            "required": "Kurs başlığı alanı zorunludur.",
-        },
-        widget=forms.TextInput(
-            attrs={"class":"form-control"}
-        )
-    )
+            "required": "Kurs başlığı alanı zorunludur."},
+        widget=forms.TextInput(attrs={"class":"form-control"}))
+    
     description = forms.CharField(widget=forms.Textarea(attrs={"class":"form-control"}))
-    imageUrl = forms.CharField(
-        widget=forms.TextInput(
-            attrs={"class":"form-control"}
-        ))
-    slug = forms.SlugField(
-        widget=forms.TextInput(
-            attrs={"class":"form-control"}
-        )
-    )
+    imageUrl = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+    slug = forms.SlugField(widget=forms.TextInput(attrs={"class":"form-control"}))
     
