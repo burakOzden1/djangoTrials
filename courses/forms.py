7d4e2536592109b7340e95ca37a4575532e6e0a0
Zeki Burak Özden <zburakozden@gmail.com>
2024-02-02 22:03:48 +0300

    modelForm yapisi ile form alanlarimizin kontrolunu yaptik.

--- courses/forms.py
+++ courses/forms.py
@@ -1,14 +1,40 @@
 from django import forms
+from courses.models import Course
 
-class CourseCreateForm(forms.Form):
-    title = forms.CharField(
-        label="Kurs Başlığı",
-        required=True,
-        error_messages={
-            "required": "Kurs başlığı alanı zorunludur."},
-        widget=forms.TextInput(attrs={"class":"form-control"}))
+# class CourseCreateForm(forms.Form):
+#     title = forms.CharField(
+#         label="Kurs Başlığı",
+#         required=True,
+#         error_messages={
+#             "required": "Kurs başlığı alanı zorunludur."},
+#         widget=forms.TextInput(attrs={"class":"form-control"}))
     
-    description = forms.CharField(widget=forms.Textarea(attrs={"class":"form-control"}))
-    imageUrl = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
-    slug = forms.SlugField(widget=forms.TextInput(attrs={"class":"form-control"}))
+#     description = forms.CharField(widget=forms.Textarea(attrs={"class":"form-control"}))
+#     imageUrl = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+#     slug = forms.SlugField(widget=forms.TextInput(attrs={"class":"form-control"}))
     
+
+class CourseCreateForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        # fields = '__all__'
+        fields = ('title', 'description', 'imageUrl', 'slug')
+        labels = {
+            'title':'Kurs Başlığı',
+            'description':'Açıklama',
+        },
+        widgets = {
+            "title": forms.TextInput(attrs={"class":"form-control"}),
+            "description": forms.Textarea(attrs={"class":"form-control"}),
+            "imageUrl": forms.TextInput(attrs={"class":"form-control"}),
+            "slug": forms.TextInput(attrs={"class":"form-control"}),
+        }
+        error_messages = {
+            "title": {
+                "required": "kurs başlığı alanı zorunludur.",
+                "max_length": "maksimum 50 karakter girilebilir",
+            },
+            "description":{
+                "required": "kurs açıklaması alanı zorunludur."
+            }
+        }
