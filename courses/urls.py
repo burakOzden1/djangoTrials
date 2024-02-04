a7ad5cda4abff66bf8c2cffe6376fe9f7816ddb8
Zeki Burak Özden <zburakozden@gmail.com>
2024-02-04 22:41:38 +0300

    Dosya upload islemi yapildi, dosyanin ozellikleri consola yazdirildi.

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, details, getCoursesByCategory, search, create_course, course_list, course_edit, course_delete
+from .views import index, details, getCoursesByCategory, search, create_course, course_list, course_edit, course_delete, upload
 
 # Hiçbir ek belirtilmediği taktirde: 127.0.0.1:8000/kurs olarak getir, yani varsayilan olarak bir sayfa belirttik.
 # ama: 127.0.0.1:8000/kurslar/list dedigimiz zamanda yine yukaridaki sayfaya gitsin.
@@ -11,6 +11,7 @@ urlpatterns = [
     path('course-list', course_list, name="course_list"),
     path('course-edit/<int:id>', course_edit, name="course_edit"),
     path('course-delete/<int:id>', course_delete, name="course_delete"),
+    path('upload', upload, name="upload_image"),
     path('<slug:slug>', details, name="course_details"),
     # path('kategori/<int:category_id>', getCoursesByCategoryId),
     path('kategori/<slug:slug>', getCoursesByCategory, name='courses_by_category'), # link alanina tanimlananlardan farkli bir deger girilirse burada bulunan dinamik alan calisir. Bu satir hep en asagida olmak zorunda.
