6145cb362e6a454ea3d255200790200aaf1c2ec8
Zeki Burak Özden <zburakozden@gmail.com>
2024-02-06 23:19:56 +0300

    Register sayfasi guncellendi ve views.py sayfasindaki user_register fonksiyonunun kodlari guncellendi.

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 def user_login(request):
     if request.user.is_authenticated:
@@ -20,7 +21,40 @@ def user_login(request):
             return render(request, "account/login.html")
 
 def user_register(request):
-    return render(request, "account/register.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        repassword = request.POST["repassword"]
+
+        if password == repassword:
+            if User.objects.filter(username = username).exists():
+                context = dict(
+                    error = "Bu isimde bir kullanıcı mevcut. Lütfen farklı bir kullanıcı ismi belirleyin.",
+                    username = username,
+                    email = email,
+                )
+                return render(request, "account/register.html", context)
+            else:
+                if User.objects.filter(email=email).exists():
+                    context = dict(
+                        error = "Bu eposta adresi kullanılıyor.",
+                        username = username,
+                        email = email,
+                    )
+                    return render(request, "account/register.html", context)
+                else:
+                    user = User.objects.create_user(username=username, email=email, password=password)
+                    user.save()
+                    return redirect("user_login")
+
+        else:
+            context = dict(
+                error = "parolalar eşleşmiyor.",
+            )
+            return render(request, "account/register.html", context)
+    else:
+        return render(request, "account/register.html")
 
 def user_logout(request):
     logout(request)
